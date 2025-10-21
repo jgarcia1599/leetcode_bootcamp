c655d04f55a9e3e0262a89f6af11a74400b609e6
Junior Garcia <junior.f.garcia99@gmail.com>
2025-10-21 12:16:10 -0600

    week 3 sols

---
+++ Week3/reorder_list.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head or not head.next or not head.next.next:
+            return
+
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev, curr = None, slow.next
+        slow.next = None
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        second = prev
+
+        first = head
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first = tmp1
+            second = tmp2
